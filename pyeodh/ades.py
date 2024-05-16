cbc0c6e7ae1b858c28eb4e1cf637a340daaef47b
Dusan Figala <dusan@oxidian.com>
2024-05-16 18:24:31 +0200

    ADES: Get jobs

--- pyeodh/ades.py
+++ pyeodh/ades.py
@@ -7,11 +7,36 @@ from typing import TYPE_CHECKING, Any
 
 from pyeodh.eodh_object import EodhObject
 from pyeodh.types import Headers, Link
+from datetime import datetime as Datetime
+
 from pyeodh.utils import join_url
 
 if TYPE_CHECKING:
     # avoids conflicts since there are also kwargs and attrs called `datetime`
     from pyeodh.client import Client
+
+
+class Job(EodhObject):
+
+    def __init__(self, client: Client, headers: Headers, data: Any):
+        super().__init__(client, headers, data)
+
+    def _set_props(self, obj: dict) -> None:
+        self.id = self._make_str_prop(obj.get("jobID"))
+        self.process_id = self._make_str_prop(obj.get("processID"))
+        self.type = self._make_str_prop(obj.get("type"))
+        self.status = self._make_str_prop(obj.get("status"))
+        self.message = self._make_str_prop(obj.get("message"))
+        self.progress = self._make_int_prop(obj.get("progress"))
+        self.links = [Link.from_dict(d) for d in obj.get("links", [])]
+        if "created" in obj:
+            self.created = Datetime.fromisoformat(obj.get("created"))
+        if "started" in obj:
+            self.started = Datetime.fromisoformat(obj.get("started"))
+        if "finished" in obj:
+            self.finished = Datetime.fromisoformat(obj.get("finished"))
+        if "updated" in obj:
+            self.updated = Datetime.fromisoformat(obj.get("updated"))
 
 
 class AdesRelType(StrEnum):
@@ -136,3 +161,15 @@ class Ades(EodhObject):
         url = join_url(self.processes_href, process_id)
         headers, response = self._client._request_json("GET", url)
         return Process(self._client, headers, response)
+
+
+    def get_jobs(self) -> list[Job]:
+        headers, response = self._client._request_json("GET", self.jobs_href)
+        if not response:
+            return []
+        return [Job(self._client, headers, item) for item in response.get("jobs", [])]
+
+    def get_job(self, job_id) -> Job:
+        url = join_url(self.processes_href, job_id)
+        headers, response = self._client._request_json("GET", url)
+        return Job(self._client, headers, response)
