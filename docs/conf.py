51256f141b7ecfcec4c2229b5fb2e3ff4a98de26
Dusan Figala <dusan@oxidian.com>
2024-06-26 12:24:04 +0200

    Release 0.0.2

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 project = "pyeodh"
 copyright = "2024, Dusan Figala"
 author = "Dusan Figala"
-release = "0.0.1"
+release = "0.0.2"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
