3feb64822a0f1849c4721871a2c35ad63140c01a
Dusan Figala <dusanfigala@protonmail.com>
2024-12-12 09:21:49 +0100

    Bump to 0.0.10

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 project = "pyeodh"
 copyright = "2024, Dusan Figala"
 author = "Dusan Figala"
-release = "0.0.9"
+release = "0.0.10"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
