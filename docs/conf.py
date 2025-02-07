dc94c6f2e43091edcaddc9473f20491517f681b1
Dusan Figala <dusan@oxidian.com>
2025-02-07 13:48:09 +0100

    Bump version to 0.0.14

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 project = "pyeodh"
 copyright = "2024, Dusan Figala"
 author = "Dusan Figala"
-release = "0.0.13"
+release = "0.0.14"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
