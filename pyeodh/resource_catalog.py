ba6b6faa67e3efba77cdc645cf704a120b98db9d
dwest77a <daniel.westwood@stfc.ac.uk>
2025-02-17 09:42:58 +0000

    Added conformance test error snippet

--- pyeodh/resource_catalog.py
+++ pyeodh/resource_catalog.py
@@ -123,6 +123,13 @@ class Item(EodhObject):
         DataPoint library to create a list of
         CloudProduct objects here."""
 
+        if not self.get_root().check_conforms_to(
+            Conformance.TRANSACTION_EXTENSION.value
+        ):
+            raise ConformanceError(
+                f"{Conformance.TRANSACTION_EXTENSION.value}",
+            )
+
         try:
             from ceda_datapoint.core.cloud import (
                 DataPointCloudProduct,
