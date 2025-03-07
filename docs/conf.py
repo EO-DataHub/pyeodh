7ca08dc39324572f0766264bc0db21ecffa66991
Dusan Figala <dusan@oxidian.com>
2025-03-07 10:54:15 +0100

    Bump to 0.1.0

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 project = "pyeodh"
 copyright = "2024, Dusan Figala"
 author = "Dusan Figala"
-release = "0.0.16"
+release = "0.1.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
--- pyeodh/__init__.py
+++ pyeodh/__init__.py
@@ -1,4 +1,4 @@
 from pyeodh.client import Client
 
-__version__ = "0.0.16"
+__version__ = "0.1.0"
 __all__ = ["Client"]
